b3d83d08994562623c94393de1d485c4176984e5
ejrose <ejrose@ncsu.edu>
2016-12-03 18:06:21 -0500

    Instructions Fixed

--- src/level2Complete.js
+++ src/level2Complete.js
@@ -7,7 +7,7 @@ var level2Complete = {
   create: function() {
       //game.add.tileSprite(0,0, game.width, game.height, 'levelFailed');
       game.add.text(320, 300, "Level 2 Complete", {fontSize: '48px', fill: "#FFF", align: "center"});
-      game.add.text(260, 380, "You Can Now Press X to Slow Your Descent", {fontSize: '24px', fill: "#FFF", align: "center"});
+      game.add.text(180, 380, "You Can Now Press the Up Arrow to Slow Your Descent", {fontSize: '24px', fill: "#FFF", align: "center"});
       game.add.text(340, 440, "Press ENTER to Start Level 3", {fontSize: '24px', fill: "#FFF", align: "center"});
   },
   update: function() {
